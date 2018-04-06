0e4fcb2c820b0964c692f855bbc27d5380df42c4
Ivan Hjelmeland <ivan.hjelmeland@shortcut.no>
2018-04-06 19:14:36 -0400

    Set map region to Oslo and added simple button to do some debugging.

--- WikiFire/ViewController.swift
+++ WikiFire/ViewController.swift
@@ -16,8 +16,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        map.setRegion(MKCoordinateRegion(center: CLLocationCoordinate2D.init(latitude: 59.922940920663507, longitude: 10.717919865631757), span: MKCoordinateSpan.init(latitudeDelta: 0.031573216609345423, longitudeDelta: 0.032186511131243378)), animated: false)
 
     }
 
+    @IBAction func lol(_ sender: Any) {
+        print("")
+    }
 }
 
