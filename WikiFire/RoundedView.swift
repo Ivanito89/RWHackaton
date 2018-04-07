9619655f0c2ad6c4ec8a668d1dcc1cc3a99f9192
Ivan Hjelmeland <ivan.hjelmeland@shortcut.no>
2018-04-06 23:47:07 -0400

    Added extension to RoundedView with fadeIn() and fadeOut() animations.

--- WikiFire/RoundedView.swift
+++ WikiFire/RoundedView.swift
@@ -34,3 +34,19 @@ class RoundedView: UIView {
     }
     
 }
+
+extension RoundedView {
+
+    func fadeIn() {
+        RoundedView.animate(withDuration: 0.1, delay: 0.0, options: UIViewAnimationOptions.curveEaseIn, animations: {
+            self.alpha = 1.0
+        }, completion: nil)
+    }
+
+    func fadeOut() {
+        RoundedView.animate(withDuration: 0.1, delay: 0.0, options: UIViewAnimationOptions.curveEaseOut, animations: {
+            self.alpha = 0.0
+        }, completion: nil)
+    }
+
+}
